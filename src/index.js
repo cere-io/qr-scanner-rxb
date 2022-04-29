7048bfbd9374e631e516d5c7bbc76d81021e96ff
Oleg-Bogatyrov <oleg.bogatyrov@timebook.net>
2022-04-29 11:46:28 +0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import {Main} from "./components";
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <Main />,
   document.getElementById('root')
 );
 
